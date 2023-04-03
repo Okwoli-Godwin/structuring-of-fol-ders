a8c028038124655aa5da2ec2a598686e4c8a3482
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-03 14:44:16 +0100

    updated

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import {createBrowserRouter } from "react-router-dom"
+import { Homelayout } from '../components'
+import Home from '../pages/Home'
 
 const Routes = () => {
+
+    const element = createBrowserRouter([
+        {
+            path: "/",
+            element: <Homelayout />,
+            children: [
+                {
+                    index: true,
+                    element: <Home />
+                }
+            ]
+        }
+    ])
+
   return (
     <div>Routes</div>
   )
